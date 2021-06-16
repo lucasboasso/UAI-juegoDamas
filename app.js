26096250187248c594d77f3d10ec88c5a95dc544
lucasboasso <lucassebastian.boasso@alumnos.uai.edu.ar>
2021-06-16 00:04:39 -0300

    Tablero realizado en JS y README

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 window.onload = function() {
     var tablero = document.getElementById("tablero");
-   // var cuadradoBlanco = document.createElement('div');
-   // var cuadradoNegro = document.createElement('div');
 
-    //tablero.appendChild(cuadradoBlanco);
-    //tablero.appendChild(cuadradoNegro);
+    /* 
+    0 = Cuadrado Blanco
+    1 = Cuadrado Negro
+    2 = Cuadrado Blanco con Ficha Blanca
+    3 = Cuadrado Blanco con Ficha Negra
+    4 = Cuadrado Negro con Ficha Blanca
+    5 = Cuadrado Negro con Ficha Negra
+    */
 
     var estadoJuego = [
         [0,1,0,1,0,1,0,1],
         [1,0,1,0,1,0,1,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0]
+        [0,1,0,1,0,1,0,1],
+        [1,0,1,0,1,0,1,0],        
+        [0,1,0,1,0,1,0,1],
+        [1,0,1,0,1,0,1,0],
+        [0,1,0,1,0,1,0,1],
+        [1,0,1,0,1,0,1,0]
     ]        
-    estadoJuego.forEach(function(row, j){
-        var cuadrado = document.createElement('div')
-
-        row.forEach(function(cell, i) {
-        
-            
-
-        if(cell === 0){
-            cuadrado.className = "cuadradoBlanco";
-        }
-        else if (cell === 1) {
-            cuadrado.className = "cuadradoNegro";
-        };
+    estadoJuego.forEach(function(row){
+        row.forEach(function(cell){
+            var cuadrado = document.createElement('div')            
+            if(cell === 0){
+                cuadrado.className = "cuadradoBlanco";
+                tablero.appendChild(cuadrado)
+            }
+            else if(cell === 1){
+                cuadrado.className = "cuadradoNegro";                
+                tablero.appendChild(cuadrado)
+            }
+            cuadrado.style.backgroundPosition
+        console.log(cuadrado)
+        })
     });
-        
-        tablero.appendChild(cuadrado);
-         });
 
 
    /* cuadradoBlanco.className = "cuadradoBlanco";
---
+++ readme.md
@@ -0,0 +1,5 @@
+Sobre Mi:
+
+Soy estudiante de la carrera de Ingenieria en Sistemas de Informacion de la Universidad Abierta Interamericana
+
+https://lucasboasso.github.io/trabajosuai/
