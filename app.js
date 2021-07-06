194ccdf87248b750fa34f178566ccc32ff755d2f
lucasboasso <lucassebastian.boasso@alumnos.uai.edu.ar>
2021-07-06 16:49:33 -0300

    probando branch test

--- app.js
+++ app.js
@@ -19,10 +19,11 @@ var estadoJuego = [
 
 function imprimirTablero(){
     estadoJuego.forEach(function(row, j){        
-        var fila = document.createElement('div');
-        fila.classList.add("fila" + (j + 1));
+        /* var fila = document.createElement('div');
+        fila.classList.add("fila" + (j + 1)); */
         row.forEach(function(cell, i){
             var celda = document.createElement('div');
+            celda.classList.add("fila" + (j + 1));
             celda.classList.add("celda" + (i + 1));
             switch(cell){
                 case 0:    
@@ -40,9 +41,9 @@ function imprimirTablero(){
                     celda.setAttribute("tabindex", 1);
                     break;
             }
-            fila.appendChild(celda);
+            tablero.appendChild(celda);
         })
-        tablero.appendChild(fila);
+        /* tablero.appendChild(fila); */
     })
 }
 
@@ -58,72 +59,79 @@ function cambioJugador(){
     }
 }
 
+function tableroClick(clickedCellEvent){
+    if(hayFichaSeleccionada(clickedCellEvent) && !clickedCellEvent.target.contains('fichaRoja')){
+        moverFicha(clickedCellEvent);
+        console.log('ya hay una marcada');
+    }
+    else{
+        selecFicha(clickedCellEvent);
+    }
+}
+
+function moverFicha(clickedCellEvent){
+    const clickedCell = clickedCellEvent.target;
+    var fila = clickedCell.classList[0]
+    var celda = clickedCell.classList[1]
+    var seleccionado = document.getElementsByClassName('seleccionado')
+
+    console.log(fila);
+    console.log(celda);
+    console.log(seleccionado);
+}
+
+function hayFichaSeleccionada(clickedCellEvent){
+    var seleccionado = document.getElementsByClassName('seleccionado')
+    if (seleccionado.length != 0){
+        seleccionado[0].classList.remove('seleccionado')      
+        selecFicha(clickedCellEvent)
+        return true;
+    }
+    else{
+        return false;
+    }        
+}
+
+function focusOut(e){
+    const element = e.target;
+    element.classList.remove('seleccionado');
+}
+
+function notAllowed(e){
+    e.classList.add("notAllowed");
+    setTimeout(function(){
+        e.classList.remove("notAllowed");
+    }, 1000);
+}
+
+function selecFicha(clickedCellEvent){
+    const clickedCell = clickedCellEvent.target;
+    var fichaRoja = clickedCell.classList.contains('fichaRoja');
+    var fichaBlanca = clickedCell.classList.contains('fichaBlanca');
+    if (clickedCell.classList.contains('seleccionado')){        
+        clickedCell.classList.remove('seleccionado');
+    }
+    else if (fichaRoja && turno === "rojas"){
+        clickedCell.classList.add('seleccionado');
+    }
+    else if (fichaBlanca && turno === "blancas"){
+        clickedCell.classList.add('seleccionado');
+    }
+    else if (fichaRoja && turno === "blancas"){
+        notAllowed(clickedCell);
+    }
+    else if (fichaBlanca && turno === "rojas"){
+        notAllowed(clickedCell);
+    }
+}
+
 window.onload = function(){
     const tablero = document.getElementById("tablero");    
     imprimirTablero();
     
     tablero.addEventListener("click", tableroClick, true);
-    tablero.addEventListener("focusout", focusOut, true);
-    function selecFicha(clickedCellEvent){
-        const clickedCell = clickedCellEvent.target;
-        var fichaRoja = clickedCell.classList.contains('fichaRoja');
-        var fichaBlanca = clickedCell.classList.contains('fichaBlanca');
-        if (clickedCell.classList.contains('seleccionado')){
-            clickedCell.classList.remove('seleccionado');
-        }
-        else if (fichaRoja && turno === "rojas"){
-            clickedCell.classList.add('seleccionado');
-        }
-        else if (fichaBlanca && turno === "blancas"){
-            clickedCell.classList.add('seleccionado');
-        }
-        else if (fichaRoja && turno === "blancas"){
-            notAllowed(clickedCell);
-        }
-        else if (fichaBlanca && turno === "rojas"){
-            notAllowed(clickedCell);
-        }
-    }
-
-    function tableroClick(clickedCellEvent){
-        if(hayFichaSeleccionada()){
-           moverFicha(clickedCellEvent);
-           alert('ya hay una marcada');
-        }
-        else{
-            selecFicha(clickedCellEvent);
-        }
-    }
-
-    function moverFicha(clickedCellEvent){
-        const clickedCell = clickedCellEvent.target;
-        var clickedCellIndex = Array.from(clickedCell.parentNode.children).indexOf(clickedCell)
-        console.log(clickedCellIndex);
-    }
-
-    function hayFichaSeleccionada(){
-        var seleccionado = document.getElementsByClassName('seleccionado').length
-        if (seleccionado != 0){
-            console.log('si, hay');
-            return true;
-        }
-        else{
-            console.log('no hay');
-            return false;
-        }        
-    }
-
-    function focusOut(e){
-        const element = e.target;
-        element.classList.remove('seleccionado');
-    }
-
-    function notAllowed(e){
-        e.classList.add("notAllowed");
-        setTimeout(function(){
-            e.classList.remove("notAllowed");
-        }, 1000);
-    }
+    /* tablero.addEventListener("focusout", focusOut, true); */
+    
 
     var cambiarTurno = document.getElementById("cambiarTurno");
     cambiarTurno.onclick = cambioJugador;
